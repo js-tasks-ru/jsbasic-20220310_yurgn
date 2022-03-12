7346125be4976e8e7bad8c520dd45c33cdc609db
yurijkov <yurgn13@gmail.com>
2022-03-12 18:12:24 +0300

    solution for 2-module/1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,8 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumSalaries = 0;
+  for (let key in salaries) {
+     if ( (typeof (salaries[key]) == "number") && !( salaries[key] == Infinity) && !( salaries[key] == -Infinity) && !( isNaN(salaries[key])))
+     { sumSalaries += salaries[key];}
+  }
+  return sumSalaries;
 }
