62202a440949d1f08e080b26cb4c45b6ae2db2ac
yurijkov <yurgn13@gmail.com>
2022-03-25 12:38:49 +0300

    solutions for 4-module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,5 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  ul.innerHTML = friends.map(friend => `<li>${friend.firstName} ${friend.lastName}</li>`).join('');
+  return ul;
 }
