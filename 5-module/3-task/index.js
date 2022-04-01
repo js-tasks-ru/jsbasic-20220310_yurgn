542c58c00dee03313ee34380d9d9975b4f729d91
yurijkov <yurgn13@gmail.com>
2022-04-01 12:20:42 +0300

    solutions for Carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -9,21 +9,15 @@ function initCarousel() {
   function scrollRight(){
     transWidth += carousel.offsetWidth; 
     carousel.style.transform = `translateX(-${transWidth}px)`;
-    console.log(buttonLeft.style.display);
-
-    if (transWidth == ((quantitySlides - 1) * carousel.offsetWidth)){
-      buttonRight.style.display = 'none';
-    }
+    if (transWidth == ((quantitySlides - 1) * carousel.offsetWidth)){buttonRight.style.display = 'none';}
     if (buttonLeft.style.display == 'none') {buttonLeft.style.display = '';}
   }
+  
   function scrollLeft(){
     transWidth -= carousel.offsetWidth; 
     carousel.style.transform = `translateX(-${transWidth}px)`;
     
-
-    if (transWidth == 0 ) {
-      buttonLeft.style.display = 'none';
-    }
+    if (transWidth == 0 ) {buttonLeft.style.display = 'none';}
     if (buttonRight.style.display == 'none') {buttonRight.style.display = '';}
   }
   buttonRight.addEventListener('click',scrollRight);
